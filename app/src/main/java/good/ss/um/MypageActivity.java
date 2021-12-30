c9d0f88c72db209c676ec07423b6ac339db417c5
senajang <57481411+senajang@users.noreply.github.com>
2021-12-30 15:40:47 +0900

    mypage에 사용자 email 표시

--- app/src/main/java/good/ss/um/MypageActivity.java
+++ app/src/main/java/good/ss/um/MypageActivity.java
@@ -6,23 +6,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MypageActivity extends AppCompatActivity {
 
-    private TextView textviewUsingTime;
+    private TextView profileEmail, textviewUsingTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user!=null) {
+            //User is signed in
+            String email = user.getEmail();
+        }
+        else {
+            //No user is signed in
+        }
+
+        profileEmail = findViewById(R.id.myemail);
+        profileEmail.setText(user.getEmail());
+
         textviewUsingTime = (TextView)  findViewById(R.id.myrenttimeis);
         RentTimeVar GlobalVar = (RentTimeVar) getApplication();
         long endTime = System.currentTimeMillis();
         long usingTime=(endTime-GlobalVar.getGlobalValue())/1000;
         textviewUsingTime.setText(usingTime/86400+"days "+usingTime%86400/3600+"h "+usingTime%3600/60+"m "+usingTime%3600%60+"s");
-        //Initialize & assign variable
 
         BottomNavigationView bottomNavigationView =findViewById(R.id.bottom_navigation);
         //set home selected
@@ -52,4 +76,6 @@ public class MypageActivity extends AppCompatActivity {
             }
         });
     }
+
+
 }
